76f6065e5a8aefb395d635634a8775e38258dc52
macbook <macbook@MacBook-Air-ihajar.local>
2025-04-18 11:55:19 +0100

    1: setup backend and frontend for FujiHire project

---
+++ backend/src/main/java/com/fujihire/backend/FujiHireApplication.java
@@ -0,0 +1,13 @@
+package com.fujihire.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujiHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FujiHireApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fujihire/backend/FujiHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujihire.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FujiHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
