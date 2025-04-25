eb3b7f9af97f3b0c247f4c8442a6421c8f231c45
macbook <macbook@MacBook-Air-ihajar.local>
2025-04-25 17:27:03 +0100

    Email Verification page

--- frontend/src/features/authentication/pages/VerifyEmail.tsx
+++ frontend/src/features/authentication/pages/VerifyEmail.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
-import { CardWrapper } from "../components/card-wrapper";
+import { z } from "zod";
+import { startTransition, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { CardWrapper } from "../components/card-wrapper";
 import { Input } from "@/components/ui/input";
 import { FormError } from "../components/form-error";
 import { FormSuccess } from "../components/form-success";
 import { Button } from "@/components/ui/button";
-
+import { useForm } from "react-hook-form";
+import { EmailValidationSchema } from "@/schemas/formSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 
 export default function VerifyEmail() {
   const [error, setError] = useState<string | undefined>("");
@@ -14,7 +18,83 @@ export default function VerifyEmail() {
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
-  
+  const form = useForm<z.infer<typeof EmailValidationSchema>>({
+    resolver: zodResolver(EmailValidationSchema),
+    defaultValues: {
+      code: "",
+    },
+  });
+
+  const validateEmail = ({ code }: z.infer<typeof EmailValidationSchema>) => {
+    startTransition(async () => {
+      setError("");
+      setSucces("");
+      setMessage("");
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `${
+            import.meta.env.VITE_API_URL
+          }/api/v1/auth/validate-email-verification-token?token=${code}`,
+          {
+            method: "PUT",
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        if (response.ok) {
+          setSucces("User validated email successfully.");
+          navigate("/");
+          return;
+        }
+        const { message } = await response.json();
+        setMessage(message);
+      } catch (error) {
+        console.log(error);
+        setError("Somethign went wrong. Please try again.");
+      } finally {
+        setIsLoading(false);
+      }
+    });
+  };
+
+  const resendEMailverificationToken = ({
+    code,
+  }: z.infer<typeof EmailValidationSchema>) => {
+    startTransition(async () => {
+      setError("");
+      setSucces("");
+      setMessage("");
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          `${
+            import.meta.env.VITE_API_URL
+          }/api/v1/auth/send-email-verification-token`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        if (response.ok) {
+          setError("");
+          setSucces("Code sent successfully. Please check your email");
+          return;
+        }
+        const { message } = await response.json();
+        setMessage(message);
+      } catch (error) {
+        console.log(error);
+        setError("Somethign went wrong. Please try again!.");
+      } finally {
+        setIsLoading(false);
+      }
+    });
+  };
+
   return (
     <div className="flex flex-col w-full min-h-screen items-center justify-center">
       <CardWrapper
@@ -23,33 +103,46 @@ export default function VerifyEmail() {
         backButtonHref="/signup"
         backButtonLabel="go back?"
       >
-        <form
-          className="flex flex-col space-y-6"
-          onSubmit={() => {}}
-        >
-          <Input
-            type="text"
-            placeholder="Verification code"
-            key="code" name="code"
-          />
-          <FormError message={error} />
-          <FormSuccess message={success} />
-          <Button
-            type="submit"
-            disabled={isLoading}
+        <Form {...form}>
+          <form
+            className="flex flex-col space-y-6"
+            onSubmit={form.handleSubmit(validateEmail)}
           >
-            {isLoading ? "...": "Validate email"}
-          </Button>
-          <Button
-            variant="outline"
-            type="button"
-            onClick={() => {}}
-            disabled={isLoading}
-          >
-            {isLoading ? "..." : "Send again"}
-          </Button>
-        </form>
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Code</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="Verification code"
+                      key="code"
+                      name="code"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormError message={error} />
+            <FormSuccess message={success} />
+            <Button type="submit" disabled={isLoading}>
+              {isLoading ? "Validating..." : "Validate email"}
+            </Button>
+            <Button
+              variant="outline"
+              type="button"
+              onClick={() => resendEMailverificationToken({ code: form.getValues("code") })}
+              disabled={isLoading}
+            >
+              {isLoading ? "Sending..." : "Send again"}
+            </Button>
+          </form>
+        </Form>
       </CardWrapper>
     </div>
-  )
+  );
 }
--- frontend/src/schemas/formSchema.ts
+++ frontend/src/schemas/formSchema.ts
@@ -9,3 +9,7 @@ export const RegisterSchema = z.object({
     email: z.string().email({ message: "Email is required" }),
     password: z.string().min(6, { message: "Minimum 6 characters required" }),
 })
+
+export const EmailValidationSchema = z.object({
+    code: z.string().min(5, "Code must be at least 5 characters long"),
+});
