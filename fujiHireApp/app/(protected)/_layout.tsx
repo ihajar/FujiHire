e4a3aefc8e884933a357eee22f4264f4e0ce9bd4
macbook <macbook@MacBook-Air-ihajar.local>
2025-05-29 20:23:14 +0100

    10: Protecting screens with auth

--- fujiHireApp/app/(protected)/_layout.tsx
+++ fujiHireApp/app/(protected)/_layout.tsx
@@ -1,11 +1,25 @@
 
 import AuthLoadingScreen from "@/components/AuthLoadingScreen";
 import { useAuth } from "@/context/AuthContext";
-import { Redirect, Stack } from "expo-router";
+import { Redirect, Stack, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
 
 
 export default function ProtectedLayout() {
   const { user, loading } = useAuth();
+  const router = useRouter();
+  const segments = useSegments();
+  
+  useEffect(() => {
+    if (loading) return;
+    const inAuthGroup = segments[0] === "(protected)";
+    if (user && !inAuthGroup) {
+      router.replace("/(protected)/(tabs)")
+    } else if(!user && inAuthGroup) {
+      router.replace("/login");
+    }
+  }, [user, loading])
+  // const inAuthGroup
   if (user === null || !user?.token) <Redirect href="/login" />
   if (loading) {
     return <AuthLoadingScreen />;
--- fujiHireApp/app/(protected)/login.tsx
+++ fujiHireApp/app/(protected)/login.tsx
@@ -1,10 +1,33 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Button, KeyboardAvoidingView, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text>login</Text>
+      <KeyboardAvoidingView behavior='padding' className='w-full px-4'>
+        <Text>Email</Text>
+        <TextInput
+          className='my-1 h-13 border-1 rounded-md p-2.5 bg-accent/20'
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize='none'
+          keyboardType='email-address'
+        />
+        <Text>Password</Text>
+        <TextInput 
+          secureTextEntry={true}
+          className='my-1 h-13 border-1 rounded-md p-2.5 bg-accent/20'
+          value={password}
+          autoCapitalize='none'
+          keyboardType='default'
+          onChangeText={setPassword}
+        />
+       <Button onPress={() => {}} title='login'/>
+      </KeyboardAvoidingView>
     </View>
   )
 }
